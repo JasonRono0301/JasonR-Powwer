3fdcb6d2191809e642f38e2871fb8046061fe308
Jason R <54982722+JasonRono0301@users.noreply.github.com>
2019-11-01 00:36:43 +0100

    Opdracht: HorecaApp
    Ben nog bezig met het maken van de HorecaApp

--- HorecaApp/HorecaApp.js
+++ HorecaApp/HorecaApp.js
@@ -1,1 +1,31 @@
-var intro = prompt("")
+var frisprijs = 2.00;
+var bierprijs = 3.00;
+var wijnprijs = 4.00;
+var bitterballenprijs = 12.00;
+
+alert ("Welkom bij de HorecaApp")
+
+alert("Type: STOP om naar de volgende bestelling te gaan.")
+
+var keuze = prompt("Welke drank wilt type een nummer in: 1 = fris of 2 = bier of 3 = wijn");
+
+function keuzes() {
+    if(keuze == 1){
+        prompt("Hoeveel aantal frisdranken wilt u?");
+    } else if(keuze == 2){
+        prompt("Hoeveel aantal bier wilt u?");
+
+    } else{
+        location.reload();
+    }
+   
+    if(keuze == 3){
+        prompt("Hoeveel aantal wijn wilt u?")
+    }
+}
+
+keuzes(keuze);
+
+
+
+
